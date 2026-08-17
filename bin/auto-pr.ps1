23b0609f88fa0d3c743e450e4e6ce3a60a3eb53a
Herman <herman@skenbe.net>
2026-08-17 13:42:25 +0200

    Change upstream parameter to hermanjustnu/scoop-comfyui

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "hermanjustnu/scoop-comfyui:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
